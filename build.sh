3ffaa4c72d0f2fad737da21ee03db7b8ada8e494
Adrian Neumann <adrian_neumann@gmx.de>
2013-10-25 16:40:57 +0200

    style, build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 for i in *.markdown; do
-	[ "$i" -nt "${i%.*}.html" ] && pandoc -c stylesheets/main.css -s -o "${i%.*}.html" "$i";
+	[ "$i" -nt "${i%.*}.html" ] && echo $i && pandoc -c stylesheets/main.css -s --latexmathml=mathml.js -o "${i%.*}.html" "$i";
 done
