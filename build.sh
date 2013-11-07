ccd88e93025f180364a391663259fa681d972a97
Adrian Neumann <adrian_neumann@gmx.de>
2013-11-07 18:26:44 +0100

    inline css

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ for i in markdown/*.markdown; do
 	html="${filename%.markdown}.html";
 	([ "$i" -nt "$html" ] || [ "template.txt" -nt "$html" ] || [ "include_after.txt" -nt "$html" ]) && 
 	echo $i && 
-	pandoc -c stylesheets/main.css -s -A include_after.txt --template=template.txt --latexmathml=mathml.js -o "$html" "$i";
+	pandoc -H stylesheets/main.css -s -A include_after.txt --template=template.txt --latexmathml=mathml.js -o "$html" "$i";
 done
