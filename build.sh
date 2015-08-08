1dac5d8cdd8fe4074ba7cda91940c3f15796428d
Adrian Neumann <adrian_neumann@gmx.de>
2015-08-08 21:17:42 +0200

    added dependence on stylesheet to build process

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 for i in markdown/*.markdown; do
 	filename="${i#markdown/}";
 	html="${filename%.markdown}.html";
-	([ "$i" -nt "$html" ] || [ "build.sh" -nt "$html" ] || [ "template.txt" -nt "$html" ] || [ "include_after.txt" -nt "$html" ]) && 
+	([ "$i" -nt "$html" ] || [ "build.sh" -nt "$html" ] || [ "template.txt" -nt "$html" ] || [ "stylesheets/main.css" -nt "$html" ] || [ "include_after.txt" -nt "$html" ]) && 
 	echo $i && 
 	pandoc -H stylesheets/main.css --smart -A include_after.txt --template=template.txt --latexmathml=mathml.js -o "$html" "$i";
 done
