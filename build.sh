b77eb627b773d3f3dccf394ddb465561e42a4661
Adrian Neumann <adrian_neumann@gmx.de>
2016-03-13 13:21:19 +0100

    changes in build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,11 @@ for i in markdown/*.markdown; do
 	echo $i && 
 	pandoc -H stylesheets/main.css --smart -A include_after.txt --template=template.txt --latexmathml=mathml.js -o "$html" "$i";
 done
+
+for i in markdown/linear_optimization/*.markdown; do
+	filename="${i#markdown/}";
+	html="${filename%.markdown}.html";
+	([ "$i" -nt "$html" ] || [ "build.sh" -nt "$html" ] || [ "template.txt" -nt "$html" ] || [ "stylesheets/main.css" -nt "$html" ] || [ "include_after.txt" -nt "$html" ]) && 
+	echo $i &&
+	pandoc -H stylesheets/main.css --smart -A include_after.txt --template=template.txt --latexmathml=../mathml.js -o "$html" "$i";
+done
